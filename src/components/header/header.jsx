428ba33f1a4bceccd948e13652c7c3e0e36966d9
dreamlixia <1763112943@qq.com>
2018-07-16 20:08:28 +0800

    有改动

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -33,7 +33,7 @@ export default class PublicHeader extends Component{
     return(
       <header className="header-container">
         <span className="header-slide-icon icon-catalog" onClick={this.toggleNav}></span>
-        <span className="header-title">{this.props.title}</span>
+        <span className="header-title">这里有改动{this.props.title}</span>
         {
           this.props.record&&<NavLink to="/record" exact className="header-link icon-jilu"></NavLink>
         }
